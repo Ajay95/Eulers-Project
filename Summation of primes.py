1d82fd9a0e93284d92dd20d8a74322d02d59592b
Ajay Babu <ajay95babu@gmail.com>
2020-03-18 04:13:14 -0700

    new summation of primes added

--- Summation of primes.py
+++ Summation of primes.py
@@ -4,4 +4,20 @@ Created on Tue Mar 17 00:45:02 2020
 
 @author: Ajay
 """
-
+if __name__ == "__main__":
+    x=[]
+    t=[]
+    p=2
+    count=0
+    
+    for i in range(2,2000000):
+        x.append(i)
+    
+    while len(x)!=0:
+        for i in x:
+            if i%p==0:
+                x.remove(i)
+        t.append(p)    
+        p=x[0]
+        count=count+1
+    print(sum(t))
