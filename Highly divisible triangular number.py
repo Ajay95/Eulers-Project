6c2cc88eb029bcd02f39783e050ed74083576ce9
Ajay Babu <ajay95babu@gmail.com>
2020-03-19 04:30:24 -0700

    new summation of primes added

---
+++ Highly divisible triangular number.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 00:56:43 2020
+
+@author: Ajay
+"""
+
---
+++ Longest Collatz sequence.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 03:36:25 2020
+
+@author: Ajay
+"""
+
